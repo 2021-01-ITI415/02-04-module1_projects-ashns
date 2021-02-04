f0ec507d09fe69942f591f5a51ab41d13e7f7253
ashns <35615496+ashns@users.noreply.github.com>
2021-02-04 17:48:15 -0500

    created appletree code

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 20f;
+    public float chanceToChangeDirection = 10;
+    public float secondBetweenAppleDrop = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
