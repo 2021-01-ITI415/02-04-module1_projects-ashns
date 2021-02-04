b62bc32f84a4a7f5f172f02176c894d19810cc0c
ashns <35615496+ashns@users.noreply.github.com>
2021-02-04 18:07:08 -0500

    tree is now moving!

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
     public GameObject applePrefab;
     public float speed = 1f;
-    public float leftAndRightEdge = 20f;
+    public float leftAndRightEdge = 10f;
     public float chanceToChangeDirection = 10;
     public float secondBetweenAppleDrop = 1;
     // Start is called before the first frame update
@@ -18,6 +19,15 @@ public class AppleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge){
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge){
+            speed = -Mathf.Abs(speed);
+        }
     }
 }
