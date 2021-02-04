d861f405d3cbea03e46ad3e9f8ce29790f7fc1a6
ashns <35615496+ashns@users.noreply.github.com>
2021-02-04 18:45:43 -0500

    game without gui created

---
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplePicker : MonoBehaviour
+{
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numBaskets; i++)
+        {
+            GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+    }
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+    }
+}
