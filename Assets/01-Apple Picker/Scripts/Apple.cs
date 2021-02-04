d45684ab91e19ecc0ef08fb3ac7ecde4cdc0d6da
ashns <35615496+ashns@users.noreply.github.com>
2021-02-04 18:35:17 -0500

    wrote apple script

---
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public static float bottomY = -20f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
