bd23c6770e341fc25132d63410fa27b6bdedb927
ashns <35615496+ashns@users.noreply.github.com>
2021-02-11 22:14:32 -0500

    made clouds

---
+++ Assets/02-Mission Demolition/Scripts/ProjectileLine.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLine : MonoBehaviour
+{
+    static public ProjectileLine S;
+    public float minDist = .1f;
+    public bool __;
+    public LineRenderer line;
+    private GameObject _poi;
+    public List<Vector3> points;
+
+    void Awake()
+    {
+        S = this;
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
+        points = new List<Vector3>();
+    }
+    public GameObject poi
+    {
+        get
+        {
+            return (_poi);
+        }
+        set
+        {
+            _poi = value;
+            if (_poi != null)
+            {
+                line.enabled = false;
+                points = new List<Vector3>();
+                AddPoint();
+            }
+        }
+    }
+    public void Clear()
+    {
+        _poi = null;
+        line.enabled = false;
+        points = new List<Vector3>();
+    }
+
+    public void AddPoint()
+    {
+        Vector3 pt = _poi.transform.position;
+        if (points.Count > 0 && (pt - lastPoint).magnitude < minDist)
+        {
+            return;
+        }
+        if (points.Count == 0)
+        {
+            Vector3 launchPosDiff = pt - Slingshot.LAUNCH_POS; 
+            points.Add(pt + launchPosDiff);
+            points.Add(pt);
+            line.positionCount = 2;
+            line.SetPosition(0, points[0]);
+            line.SetPosition(1, points[1]);
+            line.enabled = true;
+        }
+        else
+        {
+            points.Add(pt);
+            line.positionCount = points.Count;
+            line.SetPosition(points.Count - 1, lastPoint);
+            line.enabled = true;
+        }
+    }
+
+    public Vector3 lastPoint
+    {
+        get
+        {
+            if (points == null)
+            {
+                return (Vector3.zero);
+            }
+            return (points[points.Count - 1]);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (poi == null)
+        {
+            if (FollowCam.poi != null)
+            {
+                if (FollowCam.poi.tag == "Projectile")
+                {
+                    poi = FollowCam.poi;
+                }
+                else
+                    return;
+            }
+            else
+                return;
+        }
+        AddPoint();
+        if (FollowCam.poi == null)
+        {
+            poi = null;
+        }
+    }
+}
